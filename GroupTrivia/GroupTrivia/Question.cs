a3928e80008a35a10e0f61d8b456cf75ff6219b9
Brock McFarland <mcswagland@gmail.com>
2016-11-06 00:07:00 -0500

    Created GroupTrivia ASP.NET web app
    Got base functionality of being able to read a question from a file and
    send it to clients.

---
+++ GroupTrivia/GroupTrivia/Question.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupTrivia
+{
+    public class Question
+    {
+        private string _question;
+
+        private List<string> _choices;
+
+        public string question
+        {
+            get
+            {
+                return _question;
+            }
+        }
+
+        public List<string> choices
+        {
+            get
+            {
+                return _choices;
+            }
+        }
+
+        public Question(string question, List<string> options)
+        {
+            this._question = question;
+            this._choices = options;
+        }
+    }
+}
---
+++ GroupTrivia/GroupTrivia/TriviaHub.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace GroupTrivia
+{
+    public class TriviaHub : Hub
+    {
+        private static int points;
+
+        private static List<Question> questionList = new List<Question>();
+
+        private Question GetRandomQuestion()
+        {
+            Random rand = new Random();
+            int index = rand.Next(0, questionList.Count);
+            return questionList[index];
+        }
+
+        /// <summary>
+        /// Read the question list from the trivia text file and put it into a list of questions
+        /// </summary>
+        private void BuildQuestionList()
+        {
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(@"assets\trivia.txt");
+            while((line = file.ReadLine()) != null)
+            {
+                //parse question and choices
+                string[] tokens = line.Split(',');
+
+                //need at least 3 tokens (a question and 2 options) to be a valid trivia question
+                if (tokens.Length > 3)
+                {
+                    string question = tokens[0];
+                    List<string> options = new List<string>();
+                    for (var i = 0; i < tokens.Length; i++)
+                    {
+                        options.Add(tokens[i]);
+                    }
+
+                    Question q = new Question(question, options);
+                    questionList.Add(q);
+                }
+            }
+            file.Close();
+        }
+
+        /// <summary>
+        /// Get a random question and send it to clients. 
+        /// This will be private after we don't test it by calling it from client "ask" button
+        /// </summary>
+        public void GiveQuestion()
+        {
+            BuildQuestionList();
+            Question q = GetRandomQuestion();
+            Clients.All.displayQuestion(q.question, q.choices);
+        }
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
